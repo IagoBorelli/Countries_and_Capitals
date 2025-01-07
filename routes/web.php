40486fd734296f94c487650db5a0a336d93755bf
iagoborelli <iago.borelli1304@gmail.com>
2025-01-07 19:46:19 -0300

    Criação do MainController e base de dados mockada

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo 'hello World';
 });
+
+Route::get('/show_data', [MainController::class, 'showData']);
